21d6c5afcc69597bb6b27196ce15b3a9c673bc14
Rails Udemy Course <railsudemycourse@gmail.com>
2015-11-11 03:35:42 +0000

    Add bootstrap styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
   def index
+    if current_user
+      if session[:tenant_id]
+        Tenant.set_current_tenant session[:tenant_id]
+      else
+        Tenant.set_current_tenant current_user.tenants.first
+      end
+      
+      @tenant = Tenant.current_tenant
+      params[:tenant_id] = @tenant.id
+    end
   end
 end
--- config/initializers/milia.rb
+++ config/initializers/milia.rb
@@ -5,12 +5,12 @@ Milia.setup do |config|
 
 # true if you wish to use a coupon-based option in your sign-up form
 # false to otherwise not expect a coupon attribute in the parameters
-  config.use_coupon = true
+  config.use_coupon = false
 
 # true if you use recaptcha on your sign-up form; 
   # be sure to include the gem 'recaptcha' in your Gemfile
 # false if you do not
-  config.use_recaptcha = true
+  config.use_recaptcha = false
 
 # true if action after signing out is to return to the home (root) page
 # false to return to the sign-in form (devise default)
