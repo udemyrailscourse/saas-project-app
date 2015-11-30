007c5429071e65c43bd857f39989c1d060a3f5d0
Rails Udemy Course <railsudemycourse@gmail.com>
2015-11-30 20:15:21 +0000

    Add project user association

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -61,7 +61,7 @@ class ProjectsController < ApplicationController
   
   def users
     @project_users = (@project.users + (User.where(tenant_id: @tenant.id, is_admin: true))) - [current_user]
-    @other_users = @tenant.users.where(tenant_id: @tenant.id, is_admin: false) - (@project_users + [current_user])
+    @other_users = @tenant.users.where(is_admin: false) - (@project_users + [current_user])
   end
   
   def add_user
